1bfeefd5c51bdf998fb8141bdf32d59a7e724243
kkata <katayama24@up.edu>
2021-11-03 12:02:11 -0700

    Adding onTouch to humanplayer

--- app/src/main/java/com/example/scrabblegameframework/ScrabbleFramework/ScrabbleHumanPlayer.java
+++ app/src/main/java/com/example/scrabblegameframework/ScrabbleFramework/ScrabbleHumanPlayer.java
@@ -1,5 +1,6 @@
 package com.example.scrabblegameframework.ScrabbleFramework;
 
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.example.scrabblegameframework.GameFramework.GameMainActivity;
@@ -29,5 +30,15 @@ public class ScrabbleHumanPlayer extends GameHumanPlayer {
     public void setAsGui(GameMainActivity activity) {
         activity.setContentView(layoutid);
         //Set up any Click or Touch listeners here
+    }
+
+    public boolean onTouch(View v, MotionEvent event){
+        if(event.getActionMasked() == MotionEvent.ACTION_DOWN){
+            int x = (int) event.getX();
+            int y = (int) event.getY();
+            return true;
+        }
+
+        return false;
     }
 }

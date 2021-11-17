c518ca899d42ccdfce5111182c8448ca25d11370
kkata <katayama24@up.edu>
2021-11-16 19:47:04 -0800

    DumbCOmputerplayer additional fucntions

--- app/src/main/java/com/example/scrabblegameframework/ScrabbleFramework/DumbComputerPlayer.java
+++ app/src/main/java/com/example/scrabblegameframework/ScrabbleFramework/DumbComputerPlayer.java
@@ -40,23 +40,56 @@ public class DumbComputerPlayer extends GameComputerPlayer {
             }
             else {
                 Random rnd = new Random();
+                if(rnd.nextInt(4) == 0) {
                 /*(int xVal = (int) (rnd.nextInt(15));
                 int yVal = (int) (rnd.nextInt(15));
                 ScrabblePlayAction play = new ScrabblePlayAction(this, xVal, yVal);
                 xVal = (int) (rnd.nextInt(15));
                 yVal = (int) (rnd.nextInt(15));*/
 
-                for (int i = 0; i < 14; i++) {
-                    for (int j = 0; j< 15; j++){
-                        if (state.getBoard().getBoardSpace(i,j).getTile() != null){
-
-                            ScrabblePlayAction play = new ScrabblePlayAction(this, i, j+1);
-                            game.sendAction(play);
+                    for (int i = 0; i < 14; i++) {
+                        for (int j = 0; j < 15; j++) {
+                            if (state.getBoard().getBoardSpace(i, j).getTile() != null) {
+                                ScrabblePlayAction play = null;
+                                //Below
+                                if (state.getBoard().getBoardSpace(i, j - 1).getTile() != null) {
+                                    play = new ScrabblePlayAction(this, i, j - 1);
+                                }
+                                //Above
+                                else if (state.getBoard().getBoardSpace(i, j + 1).getTile() != null) {
+                                    play = new ScrabblePlayAction(this, i, j + 1);
+                                }
+                                //Left
+                                else if (state.getBoard().getBoardSpace(i - 1, j).getTile() != null) {
+                                    play = new ScrabblePlayAction(this, i - 1, j);
+                                }
+                                //Right
+                                else if (state.getBoard().getBoardSpace(i + 1, j).getTile() != null) {
+                                    play = new ScrabblePlayAction(this, i + 1, j);
+                                }
+                                if (play != null) {
+                                    game.sendAction(play);
+                                }
+                            }
                         }
                     }
-                }
-                game.sendAction(end);
+                    game.sendAction(end);
 
+                }
+                else {
+                    if (state.getSelected(playerNum).size() == 1) {
+                        ScrabbleSelectHandAction select = new ScrabbleSelectHandAction(this, 3);
+                        game.sendAction(select);
+                    }
+                    else if (state.getSelected(playerNum).size() == 2) {
+                        ScrabbleSelectHandAction select = new ScrabbleSelectHandAction(this, 6);
+                        game.sendAction(select);
+                    }
+                    else{
+                        ScrabbleExchangeAction ex = new ScrabbleExchangeAction(this);
+                        game.sendAction(ex);
+                    }
+                }
             }
         }
     }
